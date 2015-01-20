22200cdab4dbacc48d4132aa1faf6017ab215255
Tyler Youngblood <tyleryoungblood@yahoo.com>
2015-01-20 17:43:38 -0500

    add gulp-sass preprocessor

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp        = require('gulp'),
-    log         = require('gulp-util'), // display any error logs via terminal window
-    concat      = require('gulp-concat'); //concatenate files
+    gutil       = require('gulp-util'), // display any error logs via terminal window
+    concat      = require('gulp-concat'), //concatenate files
+    sass        = require('gulp-sass'); // sass to css preprocessing via gulp-sass
 
 // control the order that js files get concatenated in by the order in the array below.
 var jsSources   = [   'src/js/test.js',
@@ -15,4 +16,11 @@ gulp.task('js', function() {
   gulp.src(jsSources) //grab all the js files listed in the jsSources var
     .pipe(concat('scripts.js')) // pipe each file through the concat plugin and combine into a single "scripts.js" file
     .pipe(gulp.dest('./js/')) // copy the scripts.js file to the js folder in root
-})
+});
+
+gulp.task('sass', function() {
+  gulp.src('src/sass/*.scss')
+    .pipe(sass())
+    //.on('error', gutil.log)
+    .pipe(gulp.dest('src/css'));
+});
