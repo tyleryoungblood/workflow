2585ece82cc74617d02e49cc1a10deddb99071ce
Tyler Youngblood <tyleryoungblood@yahoo.com>
2015-01-20 17:20:46 -0500

    add ability to concatinate js files

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,18 @@
 var gulp        = require('gulp'),
-    gutil       = require('gulp-util'); // display log via terminal window
+    log         = require('gulp-util'), // display any error logs via terminal window
+    concat      = require('gulp-concat'); //concatenate files
+
+// control the order that js files get concatenated in by the order in the array below.
+var jsSources   = [   'src/js/test.js',
+                      'src/js/test2.js' ];
 
 
-gulp.task('log', function() {
-  gutil.log('Yea!');
+// gulp.task('log', function() {
+//   gutil.log('Yea!');
+// });
+
+gulp.task('js', function() {
+  gulp.src(jsSources) //grab all the js files listed in the jsSources var
+    .pipe(concat('scripts.js')) // pipe each file through the concat plugin and combine into a single "scripts.js" file
+    .pipe(gulp.dest('./js/')) // copy the scripts.js file to the js folder in root
 })
